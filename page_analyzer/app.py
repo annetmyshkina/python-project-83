fd00103e97d1c8008b5c76c4477b31335a07f706
annetmyshkina <annet.obuchova@gmail.com>
2026-01-12 09:44:23 +0300

    Add Bootstrap main page template

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,21 @@
 
-from flask import Flask
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-@app.route("/")
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "<h1>Page Analyzer</h1>"
+    if request.method == 'POST':
+        url = request.form.get('url', '')
+        return f'<h1>Анализ {url}</h1><p>Функция в разработке</p>'
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
