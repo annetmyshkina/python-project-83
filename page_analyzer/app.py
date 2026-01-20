e940717a3ceac97e4cbe74921f858af4e61eeef9
annetmyshkina <annet.obuchova@gmail.com>
2026-01-20 17:49:07 +0300

    add handlers route: urls, url

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,26 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, flash, request, redirect, url_for
+from page_analyzer.models import create_url, get_urls, get_url_by_id
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
-# КЛЮЧЕВОЕ ИЗМЕНЕНИЕ:
-app = Flask('page_analyzer', template_folder='../templates')
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app = Flask(__name__, template_folder="../templates")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        url = request.form.get('url', '')
-        return f'''
-        <div class="container mt-5">
-            <div class="alert alert-success">
-                <h4>Анализ страницы: <strong>{url}</strong></h4>
-                <p>Функционал в разработке</p>
-                <a href="/" class="btn btn-primary">← Новый анализ</a>
-            </div>
-        </div>
-        '''
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route("/urls", methods=['GET', 'POST'])
+def urls():
+    if request.method == 'POST':
+        url_input = request.form['url'].strip()
+        success, message, result = create_url(url_input)
+
+        flash(message, 'danger' if not success else 'success')
+
+        if not result:
+            return redirect(url_for('index'))
+
+        return redirect(url_for('url', url_id=result))
+
+    return render_template('urls.html', urls=get_urls())
+
+
+
+@app.route("/urls/<int:url_id>")
+def url(url_id):
+    url_data = get_url_by_id(url_id)
+    if not url_data:
+        return redirect(url_for('urls'))
+    return render_template('url.html', url=url_data)
+
+
+if __name__ == "__main__":
+    app.run()
