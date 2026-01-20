649e5ec6ee70216bcbc5de285286438702d9082e
annetmyshkina <annet.obuchova@gmail.com>
2026-01-20 17:54:53 +0300

    add url_validator.py

---
+++ page_analyzer/url_validator.py
@@ -0,0 +1,14 @@
+from validators import url
+from urllib.parse import urlparse
+
+
+def normalize_url(url_name):
+    if len(url_name) > 255 or not url(url_name):
+        return False, "Некорректный URL"
+
+    parsed = urlparse(url_name)
+    if not (parsed.scheme and parsed.netloc):
+        return False, "Некорректный URL"
+
+    domain = f"{parsed.scheme}://{parsed.netloc}"
+    return True, domain
