741dfe342d594352db5fbb6b617c936c53ba5497
annetmyshkina <annet.obuchova@gmail.com>
2026-01-21 08:44:46 +0300

    add if not exists

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-# Устанавливаем uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
-# Устанавливаем зависимости
-make install
-
-# Создаем таблицы в БД (работает как локально, так и на Render)
-psql -d $DATABASE_URL -f database.sql
+make install && psql -a -d $DATABASE_URL -f database.sql
