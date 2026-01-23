34dec5feb7e57e3be3241c294de5aa3e1679357c
annetmyshkina <annet.obuchova@gmail.com>
2026-01-23 11:58:36 +0300

    add table url_checks

--- database.sql
+++ database.sql
@@ -4,3 +4,12 @@ CREATE TABLE IF NOT EXISTS urls (
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL REFERENCES urls(id) ON DELETE CASCADE,
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
+    description TEXT,
+    created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
+);
