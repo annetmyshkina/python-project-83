5b2cc8925adaf79dbe72c19ce4c9ee3eef31864d
annetmyshkina <annet.obuchova@gmail.com>
2026-01-26 12:54:12 +0300

    add route check_url

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -11,22 +11,22 @@ url_service = URLService()
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route("/urls", methods=['GET', 'POST'])
+@app.route("/urls", methods=["GET", "POST"])
 def urls():
-    if request.method == 'POST':
-        url_input = request.form['url'].strip()
+    if request.method == "POST":
+        url_input = request.form["url"].strip()
         success, message, url_id = url_service.create_url(url_input)
 
-        flash(message, 'danger' if not success else 'success')
+        flash(message, "danger" if not success else "success")
 
         if success and url_id:
-            return redirect(url_for('url', id=url_id))
+            return redirect(url_for("url", id=url_id))
         return redirect(url_for("index"))
 
-    return render_template('urls.html', urls=url_service.get_urls_with_last_check())
+    return render_template("urls.html", urls=url_service.get_urls_with_last_check())
 
 
 
@@ -34,16 +34,19 @@ def urls():
 def url(id):
     url_data = url_service.get_url_by_id(id)
     if not url_data:
-        return redirect(url_for('urls'))
+        return redirect(url_for("urls"))
 
     checks = url_service.get_checks_url(id)
-    return render_template('url.html', url=url_data, checks=checks)
+    return render_template("url.html", url=url_data, checks=checks)
 
-@app.route("/urls/<int:id>/checks", methods=['POST'])
+@app.route("/urls/<int:id>/checks", methods=["POST"])
 def check_url(id):
-    url_service.create_check_url(id)
-    flash("Проверка создана", 'success')
-    return redirect(url_for('url', id=id))
+    if url_service.create_check_url(id):
+        flash("Проверка успешно создана", "success")
+    else:
+        flash("Произошла ошибка при проверке", "danger")
+
+    return redirect(url_for("url", id=id))
 
 if __name__ == "__main__":
     app.run()
