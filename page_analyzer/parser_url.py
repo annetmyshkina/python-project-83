a772d9cfbcda7633a3ab9b3908d5492a5ded080f
annetmyshkina <annet.obuchova@gmail.com>
2026-01-26 12:56:08 +0300

    add parser_url.py

---
+++ page_analyzer/parser_url.py
@@ -0,0 +1,25 @@
+from requests import get, RequestException
+from bs4 import BeautifulSoup
+
+def get_data(url):
+    data = {}
+    try:
+        response = get(url,  timeout=10)
+        response.encoding = "utf-8"
+        soup = BeautifulSoup(response.text, "lxml")
+
+        data["status_code"] = response.status_code
+
+        title_tag = soup.find("title")
+        data["title"] = title_tag.get_text(strip=True) if title_tag else None
+
+        h1_tag = soup.find("h1")
+        data["h1"] = h1_tag.get_text(strip=True) if h1_tag else None
+
+        desc_tag = soup.find("meta", attrs={"name": "description"})
+        data["description"] = desc_tag.get("content", "").strip() if desc_tag else None
+        return data
+
+    except RequestException as e:
+        print(f"Ошибка запроса: {e}")
+        return None
