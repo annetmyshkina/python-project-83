7652cf46b95c78f3530f16cea94be38e25f43a36
annetmyshkina <annet.obuchova@gmail.com>
2026-01-27 08:52:24 +0300

    fix path

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 curl -LsSf https://astral.sh/uv/install.sh | sh
 export PATH="$HOME/.local/bin:$PATH"
-make install
 
-if [ -n "$DATABASE_URL" ]; then
-    psql "$DATABASE_URL" -f database.sql -v ON_ERROR_STOP=1
-else
-    exit 0
-fi
-
+make install && psql -a -d $DATABASE_URL -f database.sql
